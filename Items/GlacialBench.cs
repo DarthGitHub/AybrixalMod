860af51666af8f5adf9105aa22858bb8a852c70e
Lord Pixel <45312472+FlatTreeTrunk@users.noreply.github.com>
2019-03-19 20:42:42 +0000

    Create GlacialBench.cs

---
+++ Items/GlacialBench.cs
@@ -0,0 +1,39 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AybrixalMod.Items
+{
+    public class GlacialBench : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Glacial Bench");
+            Tooltip.SetDefault("A work bench with a ice cold touch to it");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 28;
+            item.height = 14;
+            item.maxStack = 1;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.value = 150;
+            item.createTile = mod.TileType("GlacialBenchTile");
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.SnowBlock, 20);
+            recipe.AddIngredient(ItemID.IceBlock, 10);
+            recipe.AddIngredient(mod.ItemType("Permafrost Shard, 15"));
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
