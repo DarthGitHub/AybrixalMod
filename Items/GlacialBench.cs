5600d9898b1162fd7fbf904002ca9eb8a1694e26
Darth Pixel <45312472+DarthPixel-AybrixalModTerraria@users.noreply.github.com>
2019-03-23 13:01:26 +0000

    Update GlacialBench.cs

--- Items/GlacialBench.cs
+++ Items/GlacialBench.cs
@@ -1,6 +1,3 @@
-using Terraria.ID;
-using Terraria.ModLoader;
-
 namespace AybrixalMod.Items
 {
     public class GlacialBench : ModItem
@@ -8,7 +5,7 @@ namespace AybrixalMod.Items
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Glacial Bench");
-            Tooltip.SetDefault("A work bench with a ice cold touch to it");
+            Tooltip.SetDefault("A work bench with an ice cold touch to it");
         }
         public override void SetDefaults()
         {
@@ -30,7 +27,7 @@ namespace AybrixalMod.Items
             ModRecipe recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.SnowBlock, 20);
             recipe.AddIngredient(ItemID.IceBlock, 10);
-            recipe.AddIngredient(mod.ItemType("Permafrost Shard, 15"));
+            recipe.AddIngredient(mod.ItemType("PermafrostShard, 15"));
             recipe.AddTile(TileID.WorkBenches);
             recipe.SetResult(this);
             recipe.AddRecipe();
